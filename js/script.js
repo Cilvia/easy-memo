dbcf9e6b53e64d653b17509564aa1883699a5e9a
Cilvia333 <tech0970@gmail.com>
2018-06-16 18:10:28 +0900

    if hide then textarea idelete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+require('electron').remote.getCurrentWindow().on('hide',function(){
+    document.getElementById("memo").value = null;
+});
+
 function submit(){
     if(window.event.keyCode==13){
         var memo = document.getElementById("memo").value;
