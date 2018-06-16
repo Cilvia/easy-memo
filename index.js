e722662b562ec4bb6708549efc86000f79d8340e
Cilvia333 <tech0970@gmail.com>
2018-06-16 16:40:01 +0900

    add tray to quit and hide

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const {app,BrowserWindow, Menu, Tray} = require('electron')
 
-var mainWindow = null;
+var win = null;
 var tray = null;
 
 app.on('window-all-closed', function() {
@@ -13,27 +13,35 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
       width: 800,
       height: 50,
       transparent: true,
       frame: false,
-      skipTaskbar: true,
-     // resizable: false
+     // resizable: false,
+      skipTaskbar: true
     });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  win.loadURL('file://' + __dirname + '/index.html');
 
-  mainWindow.setAlwaysOnTop(true);
+  win.setAlwaysOnTop(true);
 
-  tray = new Tray(__dirname + '/img/icon.png')
+  tray = new Tray(__dirname + '/img/icon.png');
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+    {label: 'Setting', type: 'normal'},
+    {label: 'Quit', type: 'normal', role:'quit'}
+  ]);
+  tray.setHighlightMode('always');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    win.isVisible() ? win.hide() : win.show();
+  })
+  win.on('show', () => {
+    tray.setHighlightMode('always');
+  })
+  win.on('hide', () => {
+    tray.setHighlightMode('never');
+  })
 });
 
 app.on('window-all-closed', function () {
