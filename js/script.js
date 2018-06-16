dd76554ac210ad66099b6fcca1ec07045681bbc4
Cilvia333 <tech0970@gmail.com>
2018-06-16 16:24:51 +0900

    remove submit debug

--- js/script.js
+++ js/script.js
@@ -2,6 +2,5 @@ function submit(){
     if(window.event.keyCode==13){
         var memo = document.getElementById("memo").value;
         console.log(memo);
-        document.getElementById("example").value = "114514";
     }
 }
