842f4b6980f4bc8b62c407d854a8965f327dbaf2
Cilvia333 <tech0970@gmail.com>
2018-06-17 20:57:48 +0900

    add tag function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@ require('electron').remote.getCurrentWindow().on('hide',function(){
     document.getElementById("memo").value = null;
 });
 
+function dodo(text){
+    console.log(text)
+}
+
+const WordCommand = {"#dodo":dodo}
+
 function submit(){
     if(window.event.keyCode==13){
         var memo = document.getElementById("memo").value;
-        console.log(memo);
+        var words = memo.split(' ')
+        words.forEach(word => {
+            if(WordCommand[word] != null){
+                WordCommand[word](memo.replace(word,''));
+            }
+        });
     }
 }
