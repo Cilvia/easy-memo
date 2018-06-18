d8b6486df92c58a178c075ba5ad06625c2c06fd8
Cilvia333 <tech0970@gmail.com>
2018-06-18 15:48:28 +0900

    add table process

--- js/script.js
+++ js/script.js
@@ -34,7 +34,32 @@ function addCommand() {
 }
 
 function makeCommandTable(){
-    
+    Object.keys(WordCommand).forEach(function(key) {
+        var val = this[key]; // this は obj
+        addCommandTable(key)
+      }, WordCommand);
+}
+
+function addCommandTable(key){
+    var table = document.getElementById("table");
+    var row = table.insertRow(-1);
+    //td分追加
+    var cell1 = row.insertCell(-1);
+    var cell2 = row.insertCell(-1);
+    var cell3 = row.insertCell(-1);
+
+    cell1.innerHTML = '<input type="button" value=削除" id="coladd" onclick="deleteCommandTable(this)">';
+    cell2.innerHTML = key;
+    cell3.innerHTML = WordCommand[key];
+}
+
+function deleteCommandTable(obj){
+    // 削除ボタンを押下された行を取得
+    tr = obj.parentNode.parentNode;
+    delete WordCommand[tr.cells[1]];
+    updateCmdFile();
+    // trのインデックスを取得して行を削除する
+    tr.parentNode.deleteRow(tr.sectionRowIndex);
 }
 
 function loadCmdFile() {
