8d0e67ff6d651a4ca910cf76b68409aab551cdf5
Cilvia333 <tech0970@gmail.com>
2018-06-18 15:28:57 +0900

    add in-out json

--- js/script.js
+++ js/script.js
@@ -1,37 +1,51 @@
-require('electron').remote.getCurrentWindow().on('hide',function(){
+const storage = require("electron-json-storage");
+
+require("electron")
+  .remote.getCurrentWindow()
+  .on("hide", function() {
     document.getElementById("memo").value = null;
-});
+  });
 
-function makeTask(function_word){
-    return new Function("text",function_word)
+function makeTask(function_word) {
+  return new Function("text", function_word);
 }
 
-var WordCommand = {"#dodo":"console.log(text)"}
+var WordCommand = { "#log": "console.log(text)" };
 
-function submit(){
-    if(window.event.keyCode==13){
-        var memo = document.getElementById("memo").value;
-        var words = memo.split(' ')
-        words.forEach(word => {
-            if(WordCommand[word] != null){
-                makeTask(WordCommand[word])(memo.replace(word,''));
-                document.getElementById("memo").value = null;
-                return;
-            }
-        });
+function submit() {
+  if (window.event.keyCode == 13) {
+    var memo = document.getElementById("memo").value;
+    var words = memo.split(" ");
+    words.forEach(word => {
+      if (WordCommand[word] != null) {
+        makeTask(WordCommand[word])(memo.replace(word, ""));
         document.getElementById("memo").value = null;
-    }
+        return;
+      }
+    });
+    document.getElementById("memo").value = null;
+  }
 }
 
-function addCommand(){
-    WordCommand[document.addCommandForm.word.value] = document.addCommandForm.js_code.value;
-    updateCmdFile();
+function addCommand() {
+  WordCommand[document.addCommandForm.word.value] =
+    document.addCommandForm.js_code.value;
+  updateCmdFile();
 }
 
-function loadCmdFile(){
-    //WordCommand = 
+function makeCommandTable(){
+    
 }
 
-function updateCmdFile(){
-    var json = JsonStringify(WordCommand);
+function loadCmdFile() {
+  storage.get("config.json", function(error, data) {
+    if (error) throw error;
+    WordCommand = data;
+  });
+}
+
+function updateCmdFile() {
+  storage.set("config.json", WordCommand, function(error) {
+    if (error) throw error;
+  });
 }
